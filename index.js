1978693b2a79c4be20cf9f54b98119db62b08aaf
Júnior Baum <juniorsbaum@hotmail.com>
2022-10-25 21:48:42 -0300

    delete datas

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ servidor.post("/produtos", (req, res) => {
 });
 
 servidor.get("/produtos/:id", (req, res) => {
+  //listando 1 registro apenas com findOne e passando a variavel dentro
   db.findOne({ _id: req.params.id }).exec((erro, dados) => {
     if (erro) {
       console.error(erro);
@@ -51,6 +52,17 @@ servidor.put("/produtos/:id", (req, res) => {
       res.status(200).json({
         mensagem: `Produto atualizado com sucesso: ${req.params.id}`,
       });
+    }
+  });
+});
+
+servidor.delete("/produtos/:id", (req, res) => {
+  db.remove({ _id: req.params.id }, {}, (erro, registrosRemovidos) => {
+    if (erro) {
+      console.error(erro);
+    } else {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json({ registrosRemovidos });
     }
   });
 });
